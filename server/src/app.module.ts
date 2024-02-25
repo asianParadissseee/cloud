112f0f51fc81955293fc2bb2c06ac149249e3fc0
asianParadissseee <asian.paradissseee@gmail.com>
2024-02-25 23:18:28 +0600

    add create method for upload files and create storage generator

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,27 +1,31 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { FilesModule } from './files/files.module';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {UsersModule} from './users/users.module';
+import {FilesModule} from './files/files.module';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {UserEntity} from "./users/entities/user.entity";
 import {FileEntity} from "./files/entities/file.entity";
+import * as process from "process";
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'rain.db.elephantsql.com',
-      port: 5432,
-      username: 'nycxogar',
-      password: '7gnY7r-EAl0NjcS6_xWiovK52AY78LC3',
-      database: 'nycxogar',
-      entities: [UserEntity, FileEntity],
-      synchronize: true,
-    }),
-      UsersModule,
-    FilesModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot(),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.DB_HOST,
+            port: Number(process.env.DB_PORT),
+            username: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            entities: [UserEntity, FileEntity],
+            synchronize: true,
+        }),
+        UsersModule,
+        FilesModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
