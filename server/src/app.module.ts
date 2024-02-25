ed1246766f4b6e3f2576167fdc11ab8153922999
asianParadissseee <asian.paradissseee@gmail.com>
2024-02-25 21:20:09 +0600

    conenct db and create     @Column()

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { FilesModule } from './files/files.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {UserEntity} from "./users/entities/user.entity";
+import {FileEntity} from "./files/entities/file.entity";
 
 @Module({
-  imports: [UsersModule, FilesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'rain.db.elephantsql.com',
+      port: 5432,
+      username: 'nycxogar',
+      password: '7gnY7r-EAl0NjcS6_xWiovK52AY78LC3',
+      database: 'nycxogar',
+      entities: [UserEntity, FileEntity],
+      synchronize: true,
+    }),
+      UsersModule,
+    FilesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/files/entities/file.entity.ts
+++ server/src/files/entities/file.entity.ts
@@ -1,1 +1,20 @@
-export class File {}
+import {Column, DeleteDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn} from "typeorm";
+import {UserEntity} from "../../users/entities/user.entity";
+
+@Entity("file")
+export class FileEntity {
+    @PrimaryGeneratedColumn()
+    id: number
+    @Column()
+    filename: string
+    @Column()
+    originalName: string
+    @Column()
+    size: number
+    @Column()
+    mimetype: string
+    @DeleteDateColumn()
+    deleteAt?: Date
+    @ManyToOne(() => UserEntity, (user) => user.files)
+    users: UserEntity
+}
