d37b77df11aec815c4f061b04da6c2289c71e686
asianParadissseee <asian.paradissseee@gmail.com>
2024-02-25 20:49:57 +0600

    add swagger and files res

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,17 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {cors: false});
+    const config = new DocumentBuilder()
+        .setTitle('Облачное хранилище')
+        .setDescription('Данные облачного хранилища')
+        .setVersion('1.0.0')
+        .addTag('API')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
     app.enableCors({credentials: true, origin: true})
     await app.listen(3000);
 }
